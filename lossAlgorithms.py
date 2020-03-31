bd66d0a3d151ec0c828a97009734ef2f8503ad4a
skinn009 <skinner.ma@gmail.com>
2020-03-31 17:20:42 -0500

    end of day, march3

--- lossAlgorithms.py
+++ lossAlgorithms.py
@@ -13,18 +13,26 @@ class hyperGradLoss:
     self.gradTangent = computeTangent()
 
     def computeAmbient(self):
+        """
+        The gradient in the ambient space, eq. (4), Wilson, Leimeister.
+        :return: np array, (k + 1) x 1
+        """
         ambGrad = np.matmul(self.examples.T,
                          (minkowskiArrayDot(self.examples,self.centroid)**2 - 1)**-.5)
-        return amdGrad
+        return ambGrad
 
     def computeTangent(self):
+        """
+        Compute the gradient in the tangent space, Eq. (5).
+        :return: ? np array k X 1 ?
+        """
 
 
 
     def computeLoss(self):
         """
         Sum of the distances between the points and the current centroid, in hyperbolic space.
-        :return:
+        :return: scalar, which is the sum of the distances from the centroid to each of the points.
         """
         return sum(-np.arccosh(minkowskiArrayDot(self.examples, self.centroid)))
 
